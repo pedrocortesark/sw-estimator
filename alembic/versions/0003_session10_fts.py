1ed99edbcd273b488959a53352f5519d693a9d06
pedrocortes <p.cortes.nieves@gmail.com>
2026-06-24 09:21:02 +0200

    Fix migration down_revision to point to 0001_session8_pgvector

--- alembic/versions/0003_session10_fts.py
+++ alembic/versions/0003_session10_fts.py
@@ -28,7 +28,7 @@ from typing import Sequence, Union
 from alembic import op
 
 revision: str = "0003_session10_fts"
-down_revision: Union[str, None] = "0002_session8_pgvector"
+down_revision: Union[str, None] = "0001_session8_pgvector"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
